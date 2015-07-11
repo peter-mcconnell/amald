c72705c1d69ae2edfe1d0d2bbcdc33d7f5d00663
pemcconnell <pemcconnell@googlemail.com>
2015-07-11 22:15:48 +0100

    Fixing test

--- defs/defs_test.go
+++ defs/defs_test.go
@@ -1,7 +1,24 @@
 package defs
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestLazy(t *testing.T) {
-	t.Error("Write some tests!")
+var (
+	scanResults []SiteDefinition
+	olddata     Records
+)
+
+func TestSiteDefinitionsToRecords(t *testing.T) {
+	scanResults = append(scanResults, SiteDefinition{
+		Url:          "https://google.com",
+		IsLockedDown: false,
+	}, SiteDefinition{
+		Url:          "https://test.com/",
+		IsLockedDown: true,
+	})
+	records := SiteDefinitionsToRecords(scanResults)
+	if len(records.Records[0].Results) != 2 {
+		t.Errorf("Got unexpected number of records: ", len(records.Records))
+	}
 }
